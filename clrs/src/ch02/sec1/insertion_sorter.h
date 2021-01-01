3a120d796e0d41b956a6db789362fa88700ec283
Ravi <ravi.p@greyorange.sg>
2021-01-02 00:44:07 +0530

    insertion sort base fn

---
+++ clrs/src/ch02/sec1/insertion_sorter.h
@@ -0,0 +1,11 @@
+#ifndef __INSERTION_SORTER_H__
+#define __INSERTION_SORTER_H__
+
+#include <vector>
+
+class InsertionSorter {
+    public:
+        static void sort(std::vector<int>& A);
+};
+
+#endif
