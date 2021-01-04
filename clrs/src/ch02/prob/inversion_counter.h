382744bb71c6e3a0bbf59fdfe47915e85f4b3e1a
Ravi <ravi.p@greyorange.sg>
2021-01-05 00:03:34 +0530

    inversion counter iterative

--- clrs/src/ch02/prob/inversion_counter.h
+++ clrs/src/ch02/prob/inversion_counter.h
@@ -1,7 +1,11 @@
 #ifndef __INVERSION_COUNTER_H__
 #define __INVERSION_COUNTER_H__
 
+#include <vector>
+
 class InversionCounter {
+    public:
+        static int count(std::vector<int>& A);
 };
 
 #endif
