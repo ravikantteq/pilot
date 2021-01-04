d7b553ad5f5686cf400f08213c3dff727f61bd07
Ravi <ravi.p@greyorange.sg>
2021-01-04 20:21:19 +0530

    sum finder

---
+++ clrs/src/ch02/sec3/sum_finder.h
@@ -0,0 +1,10 @@
+#ifndef __SUM_FINDER_H__
+#define __SUM_FINDER_H__
+
+#include <vector>
+
+class SumFinder {
+    public:
+        static std::vector<int> two_sum_finder(std::vector<int>& A, int sum);
+};
+#endif
